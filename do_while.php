47e3f03bd4743ca650d87fcd0d8b8936ff7d3e60
shivamyadav37 <babaeminem37@gmail.com>
2020-11-10 21:29:24 +0530

    adding files

---
+++ do_while.php
@@ -0,0 +1,6 @@
+<?php
+$counter = 0;
+
+do{
+    echo $counter.'Hello';
+} while($counter<10);
--- while_loop.php
+++ while_loop.php
@@ -3,6 +3,6 @@
 $counter = 0;
 
 while ($counter<10){
-    print $counter.'Hello<br>';
+    echo $counter.'Hello<br>';
     $counter++;
 }
