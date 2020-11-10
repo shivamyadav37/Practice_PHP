298ab0b22c1ccb4188f60f4b137e192e75636e54
shivamyadav37 <babaeminem37@gmail.com>
2020-11-10 21:50:39 +0530

    adding files

---
+++ switch.php
@@ -0,0 +1,18 @@
+<?php
+$number = 1;
+
+switch ($number){
+    case 1:
+        echo 'One';
+        break;
+    case 2:
+        echo 'Two';
+        break;
+    case 3:
+        echo 'Three';
+        break;
+    default:
+        echo "Bazinga";
+        break;
+
+}
