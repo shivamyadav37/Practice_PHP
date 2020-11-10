94bbd8f74b75f269be4ef7414b5fcdc53bc8f2f1
shivamyadav37 <babaeminem37@gmail.com>
2020-11-10 20:19:35 +0530

    adding files

--- Assignment_Operators.php
+++ Assignment_Operators.php
@@ -1,6 +1,10 @@
 <?php
 $text = "Hello";
 
-$text .= " World";
+$text .= " World\n";
 
 echo  $text;
+
+$num1 = 5;
+$num1+=5;
+echo $num1;
---
+++ TripleEqual.php
@@ -0,0 +1,10 @@
+<?php
+
+$num1 = '5';
+$num2 = 5;
+if ($num1===$num2){ // Triple equal check for value as well as data type
+    echo "Equal";
+}
+else{
+    echo "Not Equal";
+}
