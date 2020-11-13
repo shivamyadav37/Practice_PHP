cc1c7cc4282956ccd39674e32afa7b9fec1a4666
shivam <babaeminem37@gmail.com>
2020-11-13 18:59:41 +0530

    adding files

--- string_function_1.php
+++ string_function_1.php
@@ -1,10 +1,12 @@
 <?php
 
-$string = "Hello Darkness my old Friend<br>";
+$string = "Hello Darkness my old Friend . <br>";
 $wc = str_word_count($string);
 $wc1 = str_word_count($string,1);
 $wc2 = str_word_count($string,2);
+$wc3 = str_word_count($string,2,'.');
 
 echo $wc;
 print_r($wc1) ;
 print_r($wc2);
+print_r($wc3);
