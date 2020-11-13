980e5a1d36f7c70927cec48792773e10ec9d0b50
shivam <babaeminem37@gmail.com>
2020-11-13 19:00:46 +0530

    adding files

--- string_function_1.php
+++ string_function_1.php
@@ -4,7 +4,7 @@ $string = "Hello Darkness my old Friend . <br>";
 $wc = str_word_count($string);
 $wc1 = str_word_count($string,1);
 $wc2 = str_word_count($string,2);
-$wc3 = str_word_count($string,2,'.');
+$wc3 = str_word_count($string,1,'.');
 
 echo $wc;
 print_r($wc1) ;
