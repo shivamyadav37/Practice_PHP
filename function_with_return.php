2116965a030dcca5b942f3e98d3cdc98a233f710
shivam <babaeminem37@gmail.com>
2020-11-13 18:58:38 +0530

    adding files

---
+++ function_with_return.php
@@ -0,0 +1,7 @@
+<?php
+
+
+function addition($num1 ,$num2){
+    return $num1+$num2;
+}
+echo addition(4,6);
---
+++ string_function_1.php
@@ -0,0 +1,10 @@
+<?php
+
+$string = "Hello Darkness my old Friend<br>";
+$wc = str_word_count($string);
+$wc1 = str_word_count($string,1);
+$wc2 = str_word_count($string,2);
+
+echo $wc;
+print_r($wc1) ;
+print_r($wc2);
